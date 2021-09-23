a714dd4f1fad3040d8e2cf68eb88e68682fb9a6f
Paulo Alves <paulorsaf@gmail.com>
2021-09-23 22:11:51 +0200

    AuthService register function Register effects

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { User } from 'src/app/model/user/User';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
+import { UserRegister } from 'src/app/model/user/UserRegister';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,19 @@ import * as firebase from 'firebase';
 export class AuthService {
 
   constructor(private auth: AngularFireAuth) { }
+
+  register(userRegister: UserRegister) : Observable<void> {
+    return new Observable<void>(observer => {
+      setTimeout(() => {
+        if (userRegister.email == "error@email.com") {
+          observer.error({message: "email already registered"});
+        } else {
+          observer.next();
+        }
+        observer.complete();
+      }, 3000)
+    })
+  }
 
   recoverEmailPassword(email: string) : Observable<void> {
     return new Observable<void>(observer => {
